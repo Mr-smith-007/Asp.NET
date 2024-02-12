7e09c5c7b20ff61d915e5cdd94e0b1518ca0ccf2
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-02-12 17:12:07 +0300

    32.5.2

--- Asp.NET/Startup.cs
+++ Asp.NET/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace Asp.NET
 {
     public class Startup
     {
-        static void About(IApplicationBuilder app)
+        private static void About(IApplicationBuilder app)
         {
             app.Run(async context =>
             {
@@ -27,16 +28,14 @@ namespace Asp.NET
                 await context.Response.WriteAsync($"App name: {env.ApplicationName}. App running configuration: {env.EnvironmentName}");
             });
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()|| env.IsStaging())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -44,7 +43,19 @@ namespace Asp.NET
             app.UseRouting();
 
             app.Use(async (context, next) =>
-            {                
+            {
+                string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+
+                string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
+
+                await File.AppendAllTextAsync(logFilePath, logMessage);
+
+                await next.Invoke();
+            });
+
+
+            app.Use(async (context, next) =>
+            {
                 Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
                 await next.Invoke();
             });
@@ -68,7 +79,7 @@ namespace Asp.NET
 
         }
 
-        
+
     }
 
 
